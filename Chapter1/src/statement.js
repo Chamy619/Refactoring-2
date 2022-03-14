faed69133775f203fd80518f847fb191952eae68
Chamy619 <yd95069506@naver.com>
2022-03-14 17:33:13 +0900

    :hammer: totalVolumeCredits 함수 옮기기

--- Chapter1/src/statement.js
+++ Chapter1/src/statement.js
@@ -3,6 +3,7 @@ function statement(invoice, plays) {
   statementData.customer = invoice.customer;
   statementData.performances = invoice.performances.map(enrichPerformance);
   statementData.totalAmount = totalAmount(statementData);
+  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
   return renderPlainText(statementData, plays);
 
   function enrichPerformance(aPerformance) {
@@ -56,6 +57,14 @@ function statement(invoice, plays) {
     }
     return result;
   }
+
+  function totalVolumeCredits(data) {
+    let result = 0;
+    for (let perf of data.performances) {
+      result += perf.volumeCredits;
+    }
+    return result;
+  }
 }
 
 function renderPlainText(data) {
@@ -66,16 +75,8 @@ function renderPlainText(data) {
   }
 
   result += `총액: ${usd(data.totalAmount)}\n`;
-  result += `적립 포인트: ${totalVolumeCredits()}\n`;
+  result += `적립 포인트: ${data.totalVolumeCredits}\n`;
   return result;
-
-  function totalVolumeCredits() {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.volumeCredits;
-    }
-    return result;
-  }
 
   function usd(aNumber) {
     return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }).format(
