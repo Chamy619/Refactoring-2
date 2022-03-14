94832ffc2d8d23547b313295d382292fd6ed3b55
Chamy619 <yd95069506@naver.com>
2022-03-14 15:12:37 +0900

    :hammer: 적립 포인트 계산하는 부분 volumeCreditsFor 함수로 추출

--- Chapter1/src/statement.js
+++ Chapter1/src/statement.js
@@ -6,10 +6,7 @@ function statement(invoice, plays) {
     .format;
 
   for (let perf of invoice.performances) {
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    if (playFor(perf).type === 'comedy') {
-      volumeCredits += Math.floor(perf.audience / 5);
-    }
+    volumeCredits += volumeCreditsFor(perf);
 
     result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience}석)\n`;
     totalAmount += amountFor(perf);
@@ -43,6 +40,15 @@ function statement(invoice, plays) {
         throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
     }
 
+    return result;
+  }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if (playFor(aPerformance).type === 'comedy') {
+      result += Math.floor(aPerformance.audience / 5);
+    }
     return result;
   }
 }
