429dfc3655c2f473bcccde92cfb6c9bb36c98976
양채훈 <chamy@yangchaehun-ui-MacBookAir.local>
2022-03-17 22:15:34 +0900

    :hammer: 테스트코드 중복 제거

--- Chapter04/test/test.spec.js
+++ Chapter04/test/test.spec.js
@@ -3,13 +3,16 @@ import chai from 'chai';
 const expect = chai.expect;
 
 describe('province', function () {
+  let asia;
+  beforeEach(function () {
+    asia = new Province(sampleProvinceData());
+  });
+
   it('shortfall', function () {
-    const asia = new Province(sampleProvinceData());
     expect(asia.shortfall).equal(5);
   });
 
   it('profit', function () {
-    const asia = new Province(sampleProvinceData());
     expect(asia.profit).equal(230);
   });
 });
